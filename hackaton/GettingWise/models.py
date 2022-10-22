d602b362ca1c99623c04a2d4ea49d2744c8ec80e
l3xi42 <lala.schnitzer@gmail.com>
2022-10-22 14:34:59 +0200

    minor changes

--- hackaton/GettingWise/models.py
+++ hackaton/GettingWise/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms.models import modelform_factory
 
 # Create your models here.
 class known_vendors(models.Model):
@@ -10,8 +11,7 @@ class known_vendors(models.Model):
 
 
 class item(models.Model):
-    #receipt_id, date, cash_register, total, place, merchant name
-    main_id = models.IntegerField()
+    #receipt_name, date, cash_register, total, place, merchant name
     receipt_name = models.CharField(max_length=200)
     date = models.DateTimeField('date published')
     cash_register = models.CharField(max_length=100)
@@ -19,5 +19,16 @@ class item(models.Model):
     merchant_name = models.ForeignKey(known_vendors, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.main_id
+        return self.receipt_name
     
+"""
+form = modelform_factory(, fields=('gender', 'gender_na', 'height', 'height_na',))
+populated_form = form(data=my_json)
+
+if populated_form.is_valid():
+    populated_form.save()
+else:
+    populated_form.errors = '-1'
+    populated_form.save()
+    
+"""
